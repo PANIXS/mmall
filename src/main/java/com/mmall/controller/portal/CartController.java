85ff0e1752d12ed7227ce1dd845d5004a180bb7c
Li Pan <1160728236@qq.com>
2018-03-26 18:32:30 +0800

    s

--- src/main/java/com/mmall/controller/portal/CartController.java
+++ src/main/java/com/mmall/controller/portal/CartController.java
@@ -1,10 +1,29 @@
 package com.mmall.controller.portal;
 
+import com.mmall.common.Const;
+import com.mmall.common.ResponseCode;
+import com.mmall.common.ServerResponse;
+import com.mmall.pojo.User;
+import com.mmall.service.ICartService;
+import com.mmall.vo.CartVo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/cart/")
 public class CartController {
-    
+
+    @Autowired
+    private ICartService iCartService;
+
+    public ServerResponse<CartVo> add(HttpSession session, Integer count, Integer productId){
+        User user = (User) session.getAttribute(Const.CURRENT_USER);
+        if (user == null) {
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(), "用户未登录,请登录管理员");
+        }
+        return iCartService.add(user.getId(),productId,count);
+    }
 }
